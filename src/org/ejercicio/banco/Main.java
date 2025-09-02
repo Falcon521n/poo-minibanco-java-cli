5793caa2ba1582a5748fdcf4bbc7cb1518435bea
Falcon521n <falconalexander480@gmail.com>
2025-09-02 18:14:50 +0000

    Nueva Linea

--- src/org/ejercicio/banco/Main.java
+++ src/org/ejercicio/banco/Main.java
@@ -4,6 +4,8 @@ public class Main {
     public static void main(String[] args) {
         Cliente cliente1 = new Cliente("Yair Falcon", 123123);
         CuentaBancaria cuenta1 = new CuentaBancaria(123123,cliente1,10000);
+
+        CuentaBancaria  cuenta2 = new CuentaBancaria(123, new Cliente("David Coba",234),1200);
     }
     
 }
